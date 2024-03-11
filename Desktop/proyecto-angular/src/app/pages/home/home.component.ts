e756247ef44f77d4c41c49db917f378e31530c98
Fridejr <antoniotorodaw@gmail.com>
2024-03-11 05:41:50 +0100

    home & footer finished + some changes

--- Desktop/proyecto-angular/src/app/pages/home/home.component.ts
+++ Desktop/proyecto-angular/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/* import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -24,4 +24,31 @@ export class HomeComponent implements OnInit {
     return `¡Bienvenido a ${this.appName}! Aquí aprenderás sobre Angular, Bootstrap y FontAwesome de manera fácil y divertida.`;
   }
 
+}
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports : [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  visitCount: number = 0;
+  appName: string = 'Mi Rincón de Aprendizaje';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // No incrementamos el contador al inicializar el componente
+  }
+
+  incrementVisitCount() {
+    // Incrementamos el contador de visitas al hacer clic en el botón
+    this.visitCount += 1;
+  }
 }
